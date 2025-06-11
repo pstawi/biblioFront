579d3b7709ab1b7dbb485f80f79936e87e40d700
pierreStawi <pstawikowski@fcdigital.fr>
2025-06-11 15:01:52 +0200

    oui-oui

--- document.js
+++ document.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const form = document.getElementById('formDocument');
+
+
+
+    // fonction tableau document
     async function loadDocuments(){
         try {
             const urlGetDocuments = await fetch("http://localhost:3000/api/documents");
@@ -27,5 +32,63 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // fonction remplissage select type modal
+    async function loadType() {
+    try {
+      const urlGetType = await fetch("http://localhost:3000/api/type");
+      console.log("urlGetType", urlGetType);
+
+      const data = await urlGetType.json();
+
+      //  console.log(data);
+      const typeList = document.getElementById("selectType");
+
+      data.forEach((key) => {
+        const option = document.createElement("option");
+        option.textContent = key.libelle;
+        option.value = key.idType
+        typeList.appendChild(option);
+
+        //  console.log(key.prenom);
+      });
+
+      // console.log(data.clients);
+    } catch (error) {
+      console.error("erreur de chargement des types:", error);
+    }
+  }
+
+  form.addEventListener('submit', async function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+    await addDocument();
+});
+
+  async function addDocument(){
+    const titre = document.getElementById('titre').value;
+    const resume = document.getElementById('resume').value;
+    const date = document.getElementById('date').value;
+    const idType = document.getElementById('selectType').value;
+
+    console.log(titre, resume, date, idType);
+
+    try {
+
+        const urlAddDocument = await fetch('http://localhost:3000/api/addDocument', {
+            method: 'POST',
+            headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({titre, resume, date, idType})
+        });
+
+        loadDocuments();
+        
+    } catch (error) {
+        console.error(error);
+        
+        
+    }
+  }
     loadDocuments(); // Appel initial pour charger les documents
+    loadType();
 });
